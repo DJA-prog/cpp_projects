2ae6432fde7b64bdfd0de7c7df32b8b9c6b8e5a0
Dino Jose Almirall <almirall.dino@gmail.com>
2022-03-08 21:37:13 +0200

    update

--- write csv/main.cpp
+++ write csv/main.cpp
@@ -24,17 +24,22 @@ int main(void)
     string find_phrase = "Student23";
     cin >> find_phrase;
 
-    cout << "0-Computer Name" << endl << "1-username" << endl << "2-IP" << endl << "3-password" << endl << "4-updated" << endl << "5-all" << endl;
+    cout << "0-Computer Name" << endl << "1-username" << endl << "2-IP" << endl << "3-password" << endl << "4-updated" << endl;
 
     cout << "Return (0 - 5): ";
     int csv_column = 4;
     cin >> csv_column;
 
+    cout << "New Value: ";
+    string new_value;
+    cin >> new_value;
+
     fstream csv_file;
     csv_file.open("list.csv", ios_base::in | ios_base::out);
 
     unsigned char ch;
-    string csv_content;
+    string csv_content = "";
+    string new_csv_content = "";
     if(!csv_file)
         return 1;
 
@@ -49,12 +54,16 @@ int main(void)
     int NL_1 = 0;
     int NL_2 = 0;
     string line = "";
+    string new_line = "";
     string *split_return;
+    int row = 0;
+
     while (true)
     {
         NL_2 = csv_content.find(0x0A, NL_1 + 1);
-        if (NL_2 == -1)
+        if (NL_2 == string::npos)
             break;
+        row++;
         line = csv_content.substr(NL_1, NL_2 - NL_1);
         if(line.find(0x0A, 0) != string::npos)
             line.erase(line.find(0x0A, 0),1);
@@ -64,20 +73,20 @@ int main(void)
             // cout << line << endl;
             // cout << line.substr(line.find_last_of(0x2C) + 1, line.length() - line.find_last_of(0x2C)) << endl;
             split_return = split(line, 0x2C);
-            if(csv_column == 5)
+            // int value_length = split_return[csv_column].length();
+            for (int i = 0; i < 5; i++)
             {
-                for (int i = 0; i < 5; i++)
+                if(i == csv_column){
+                    new_line += new_value;
+                }else
                 {
-                    cout << split_return[i] << (char)0x09;
+                    new_line += split_return[i];
                 }
-                cout << (char)0x0A;
-                
-            }else
-            {
-                cout << split_return[csv_column] << endl;
             }
+            cout << new_line << " at " << row << endl;
         }
         NL_1 = NL_2;
     }
+    
     
 }
